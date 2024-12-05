41a556d69012fee0759c65a3195f26da5cb1bfaa
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-05 18:23:50 -0300

    update image

--- src/components/home-image.tsx
+++ src/components/home-image.tsx
@@ -5,7 +5,7 @@ export function HomeImage() {
     <div className="hidden md:block relative w-64 h-64 lg:w-96 lg:h-96">
       <Image
         alt="ferramentas"
-        src={"/images/ferramentas.webp"}
+        src={"/images/ferramentas.png"}
         fill
         className="absolute object-cover rounded-full"
       />
