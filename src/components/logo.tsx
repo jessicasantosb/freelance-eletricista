9ecdf8cffbc9b9c1d26ecaf5e526173690be650c
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-05 15:45:43 -0300

    change logo color

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,15 +2,11 @@ import Image from "next/image";
 
 export function Logo() {
   return (
-    <div className="flex items-center gap-2 ">
-      <Image
-        alt="logo"
-        src={"/images/logo.svg"}
-        width={40}
-        height={25}
-        className="[&>*]:text-muted-foreground"
-      />
-      <h4 className="tracking-wider">VALDIR MARQUES</h4>
+    <div className="flex items-center gap-2">
+      <Image alt="logo" src={"/images/logo.svg"} width={40} height={25} />
+      <h4 className="tracking-wider text-[#a9acb1] dark:text-[#d1d5db9f]">
+        VALDIR MARQUES
+      </h4>
     </div>
   );
 }
