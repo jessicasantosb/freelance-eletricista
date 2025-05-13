828ad0f4d6d9a138c5828d6c065a7f6c1fe8eddb
Jessica <jessica.santosb@outlook.com>
2025-05-13 08:53:27 -0300

    style: change home texts size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,45 @@
-import {HomeMessage } from "@/components";
+import { PlugZap } from "lucide-react";
+import Image from "next/image";
 
-export default async function Home() {
+import { MessageTrigger } from "@/components";
+
+export default function Home() {
   return (
-    <main className="w-full p-4 md:px-6">
-      <div className="my-14 flex gap-6">
-        <HomeMessage />
+    <div className="w-full p-6">
+      <div className="mb-6 flex flex-col lg:flex-row items-center">
+        <div className="flex flex-col justify-between items-center lg:items-start">
+          <h1 className="pb-4 lg:pb-0 uppercase font-bold flex flex-col items-center gap-2 lg:items-start text-2xl md:text-5xl md:tracking-widest">
+            Especialista em Serviços de{" "}
+            <span className="text-secondary md:tracking-widest text-5xl md:text-8xl flex items-center">
+              <PlugZap className="size-10 md:size-20" /> Eletricidade
+            </span>
+            <span className="md:tracking-tighter text-4xl md:text-8xl ">
+              {" "}
+              em Belo Horizonte
+            </span>
+          </h1>
+
+          <h3 className="text-xl lg:text-2xl text-muted-foreground text-center lg:text-left flex flex-col">
+            <span>
+              Os melhores serviços em elétrica e instalações residenciais com
+              preço justo!
+            </span>
+            <span>
+              Confiança e segurança para resolver qualquer problema elétrico.
+            </span>
+          </h3>
+        </div>
+
+        <div className="relative size-64 lg:size-[29.5rem] select-none">
+          <Image
+            alt="ferramentas"
+            src={"/images/avatar.png"}
+            fill
+            className="absolute object-cover rounded-full"
+          />
+        </div>
       </div>
-    </main>
+      <MessageTrigger />
+    </div>
   );
 }
